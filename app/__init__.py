26ae8d2a4f7e0d7297c4361629f1a5b0fd463f5f
agusbalestra <agusbalestra0@gmail.com>
2023-08-24 19:55:53 -0300

    app setup. Ref #7

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from config import config
 
-app = Flask(__name__)
+def create_app(config_name="development"):
+  app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+  app.config.from_object(config[config_name])
+  config[config_name].init_app(app)
+
+  @app.route("/")
+  def hello_world():
     return "<p>Hello, World!</p>"
 
+  return app
