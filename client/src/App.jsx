560482d92218d7f236a54966f2e60d68058c9e4b
Agustin Lopez <agustinlopezprieto@gmail.com>
2023-09-30 23:18:55 -0300

    Add basic structure Ref. #25

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,73 @@
+import { useState } from "react"
+
+//futuro componente celda
+const Cell = ({ updateBoard, alien, row, col, elem }) => {
+  const handleClick = () => {
+    updateBoard(row, col, elem);
+  }
+
+  return (
+    <div onClick={handleClick} className="cell" row={row} col={col}>
+      {elem && 
+        <Alien/>
+      }
+    </div>
+  )
+}
+
+const Alien = () => {
+  //const className = `${hayAlien ? 'alien' : ''}`
+  return (
+    <div className="alien"/>
+  )
+}
 
 function App() {
 
+  const [winner, setWinner] = useState(null)
+  const [alien, setAlien] = useState(false)
+  
+  const grid = [Array(3).fill(false),
+                Array(3).fill(false),
+                Array(3).fill(false)]
+  
+  const [board, setBoard] = useState(grid)
+  console.log(board)
+  
+  const updateBoard = (row, col, elem) => {
+    console.log(board)
+    const newBoard = [...board]
+    newBoard[row][col] = !elem
+    console.log(board)
+    setBoard(newBoard)
+  }
+
   return (
     <>
-      <h1>Hola</h1>
+      <h1>Earth conquest</h1>
+      <section className="board">
+        {
+          board.map((row, i) => {
+            return (
+                row.map((cell, j) => {
+                  return (
+                  <Cell 
+                    key={j}
+                    col={j}
+                    row={i}
+                    elem={board[i][j]}
+                    updateBoard={updateBoard}>
+                      {cell}
+                  </Cell>
+                  )
+                })
+            )
+          })
+        }
+      </section>
+      <section className="modifiers">
+        
+      </section>
     </>
   )
 }
