e088e2d9ac8bf4da247c3e14ccc4dca9af41c41d
Ivan Peruchin <ivanperuchin12@gmail.com>
2023-10-04 15:38:47 -0300

    add alterator state Ref. #51

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,49 @@
 import { useState } from "react"
 import data from "./data.json"
-import {modifier} from "./constants.js"
+import unitario from "./unitario.json"
+import {alterator, modifier} from "./constants.js"
 
 
 
 //leer el js 
-console.log(data.grid)
+console.log(unitario.grid)
 
 //futuro componente celda
-const Cell = ({ updateBoard, aliens, row, col, elem, cell_modifier, cell_alterator}) => {
+const Cell = ({ updateBoard, row, col, children}) => {
   const handleClick = () => {
-    updateBoard(row, col, alien);
+    updateBoard(row, col);
   }
-
+ 
   return(
-   <div className="cell" row={row} col={col}>
+   <div onClick={handleClick} className="cell" row={row} col={col}>
       { 
-        <Content aliens={aliens} cell_modifier={cell_modifier} cell_alterator={cell_alterator}/>
+        <Content aliens={children.aliens} cell_modifier={children.modifier} cell_alterator={children.alterator}/>
       }
+      
     </div>
   )
 
 }
 
-const Content = ({ aliens, cell_modifier, cell_alterator}) =>{
+
+
+const Content = ({aliens, cell_modifier, cell_alterator}) =>{
 
   if (aliens.length !== 0) {
     return(
-    <Alien/>
+     <Alien/>
     )
-    
-  } 
+  }
+   
   if (cell_modifier === "modifier.mountain"){
     return(
-    <Modifier/>
+      <Modifier/>
     )
   }
 
-  if (cell_alterator === null){
+  if (cell_alterator !== null){
     return(
-    <Alterator/>
+      <Alterator/>
     )
   }
 }
@@ -52,33 +56,54 @@ const Alien = () => {
 }
 
 const Modifier = () => {
-  //const className = `${hayAlien ? 'alien' : ''}`
   return (
+    //<div className="modifier"/>
     <div> M </div>
   )
 }
 
-const Alterator = () => {
-  //const className = `${hayAlien ? 'alien' : ''}`
+const Alterator = ({setAlter}) => {
+  //agrega un alterador seleccionado por el usuario, para despues setearlo al tablero
+  const agregarAlt = () => {
+    setAlter(alterator.trap)
+  }
+
   return (
-    <div> A </div>
+    //<div className="alterator"/>
+    <div onClick={agregarAlt}> A </div>
   )
 }
+
+
+
 
 function App() {
 
   const [winner, setWinner] = useState(null)
   const [alien, setAlien] = useState(false)
+  const [alter, setAlterator] = useState(null)
   
-  
+  //setea el board con la grilla del JSON
   const [board, setBoard] = useState(data.grid)
   
-  const updateBoard = (row, col, elem) => {
+  const setAlter = (newAlterator) => {
+    setAlterator(newAlterator)
+  }
+
+  const updateBoard = (row, col) => {
+    if (alter === null) return
+    if (board[row][col].alterator !== null) return
     const newBoard = [...board]
-    newBoard[row][col] = !elem
+    const newAlterator = alter
+    
+    newBoard[row][col].alterator =  newAlterator
+    
     setBoard(newBoard)
   }
 
+  
+
+  console.log(alter)
   return (
     <>
       <h1>Earth conquest</h1>
@@ -92,9 +117,6 @@ function App() {
                     key={j}
                     col={j}
                     row={i}
-                    aliens={cell.aliens}
-                    cell_modifier={cell.modifier}
-                    cell_alterator={cell.alterator}
                     updateBoard={updateBoard}>
                       {cell}
                   </Cell>
@@ -103,12 +125,16 @@ function App() {
             )
           })
         }
-      </section>
+      </section>      
+      
+      
       <section className="modifiers">
-        <h1>Modificadores</h1>
+        <h1>Modificadores</h1> 
+        <div className="alter"> <Alterator setAlter={setAlter} /> </div>
         
       </section>
     </>
+    //TODO: como seleccionar diferentes alteradores y pasarlo como parametros
   )
 }
 
