ff131f4dcb1847e99d9b48076d0bada2b5f1fdbe
matybq <matybq20@gmail.com>
2023-10-09 19:02:56 -0300

    api squeleton. Ref #64

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from config import config
+from flask_cors import CORS
+
+from app.backend.blueprints import games_bp
 
 
 def create_app(config_name="development"):
+    # app stuff
     app = Flask(__name__)
+    CORS(app)
+    register_modules(app)
 
+    # config stuff
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
-    @app.route("/")
-    def hello_world():
-        return "<p>This is EARTH CONQUEST!</p>"
-
     return app
+
+
+# blueprints register
+def register_modules(app):
+    app.register_blueprint(games_bp, url_prefix='/games')
