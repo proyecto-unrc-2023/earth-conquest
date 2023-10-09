f38a0837a0831ad7383f77eeb3adab59fac0b00e
Ivan Peruchin <ivanperuchin12@gmail.com>
2023-10-09 17:47:00 -0300

    Add aliens with eyes Ref. #63

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import {alterator, modifier} from "./constants.js"
 //leer el js 
 console.log(data2.grid)
 
-//futuro componente celda
+//Componente Celda
 const Cell = ({ updateBoard, row, col, children, blue_base, green_base }) => {
   const handleClick = () => {
     updateBoard(row, col);
@@ -14,122 +14,113 @@ const Cell = ({ updateBoard, row, col, children, blue_base, green_base }) => {
 
   return(
    <div onClick={handleClick} className="cell" row={row} col={col}>
-      {<>
-        {
-          children.modifier && <Modifier type={children.modifier}/>   
-        }
-        
-        {          
-          children.aliens.map((alien, index) => {
-          return(
-            <Alien key={index} color={alien.team}></Alien>
-          )
-        })
-        }
+      {
+        <>
+          {
+            children.modifier && <Modifier type={children.modifier}/>   
+          }
+          
+          {          
+            children.aliens.map((alien, index) => {
+            return(
+              <Alien key={index} team={alien.team} eyes={alien.eyes}></Alien>
+            )
+          })
+          }
 
-        { children.alterator && <Alterator tipo={children.alterator}/> }
+          { children.alterator && <Alterator tipo={children.alterator}/> }
 
-        {
-          (row <= blue_base.x && col <= blue_base.y ) && <Base/>   
-        }
-        {
-          (row >= green_base.x && col >= green_base.y) && <Base/>   
-        }
-      </>}
-      
+          {
+            (row <= blue_base.x && col <= blue_base.y ) && <Base/>   
+          }
+          {
+            (row >= green_base.x && col >= green_base.y) && <Base/>   
+          }
+        </>
+      }
     </div>
   )
 }
 
+//Componente Base
 const Base = () => {
   return (
-    <div className="Base"> Base</div>
+    <div className="base"> Base</div>
   )
 }
 
-const Alien = ({color}) => {
-  if (color === "blue"){
-    return (
+//Componente Alien
+const Alien = ({team, eyes}) => {
+    return (      
       <div className="alien">
-        <img src={"../public/blue_alien.png"} className="img_blue_alien" alt="" />
+        {((team === "blue") && (eyes === 1)) && <img src={"../public/blue_alien_1.png"} className="img_blue_alien" alt="" />}
+        {((team === "blue") && (eyes === 2)) && <img src={"../public/blue_alien_2.png"} className="img_blue_alien" alt="" />}
+        {((team === "blue") && (eyes === 3)) && <img src={"../public/blue_alien_3.png"} className="img_blue_alien" alt="" />}
+        {((team === "blue") && (eyes === 4)) && <img src={"../public/blue_alien_4.png"} className="img_blue_alien" alt="" />}
+        {((team === "blue") && (eyes === 5)) && <img src={"../public/blue_alien_5.png"} className="img_blue_alien" alt="" />}
+
+        {((team === "green") && (eyes === 1)) && <img src={"../public/green_alien_1.png"} className="img_blue_alien" alt="" />}
+        {((team === "green") && (eyes === 2)) && <img src={"../public/green_alien_2.png"} className="img_blue_alien" alt="" />}
+        {((team === "green") && (eyes === 3)) && <img src={"../public/green_alien_3.png"} className="img_blue_alien" alt="" />}
+        {((team === "green") && (eyes === 4)) && <img src={"../public/green_alien_4.png"} className="img_blue_alien" alt="" />}
+        {((team === "green") && (eyes === 5)) && <img src={"../public/green_alien_5.png"} className="img_blue_alien" alt="" />}
       </div>
     )
-  } else {
-    return (
-      <div className="alien">
-        <img src={"../public/green_alien.png"} className="img_green_alien" alt="" />
-      </div>
-    )
-  }
 }
 
-const Modifier = ({type}) => {
-  if(type === modifier.mountain){
-    return (
-      <div className="modifier"> 
-        <img src={"../public/mountain.png"} className="img_mountain" alt="" />
-      </div>
-    )
-  }
-  if(type === modifier.killer){
-    return (
-      <div className="modifier"> 
-        <img src={"../public/killer.png"} className="img_killer" alt="" />
-      </div>
-    )
-  }
-  if(type === modifier.multiplier){
-    return (
-      <div className="modifier"> 
-        <img src={"../public/multiplier.png"} className="img_multiplier" alt="" />
-      </div>
-    )
-  }
+//Componente Modificador
+const Modifier = ({type}) => { 
+  return (
+    <div className="modifier"> 
+      {
+        (type === modifier.mountain) &&  <img src={"../public/mountain.png"} className="img_mountain" alt="" />
+      }
+      {
+        (type === modifier.killer) && <img src={"../public/killer.png"} className="img_killer" alt="" />
+      }
+      {
+        (type === modifier.multiplier) && <img src={"../public/multiplier.png"} className="img_multiplier" alt="" />
+      }
+    </div>
+  )
 }
 
+//Componente Alterador
 const Alterator = ({setAlter, tipo}) => {
   //agrega un alterador seleccionado por el usuario, para despues setearlo al tablero
   const agregarAlt = () => {
     setAlter(tipo)
   }
 
-  if(tipo === alterator.trap){
-    return (
-      <div className="alterator" onClick={agregarAlt}> 
-        <img src={"../public/trap.png"} className="img_trap" alt="" />
-      </div>
-    )
-  }
-  if(tipo === alterator.directioner){
-    return (
-      <div className="alterator" onClick={agregarAlt}> 
-        <img src={"../public/directioner.png"} className="img_directiorer" alt="" />
-      </div>
-    )
-  }
-  if(tipo === alterator.teleport){
-    return (
-      <div className="alterator" onClick={agregarAlt}> 
-        <img src={"../public/teleporter.gif"} className="img_teleporter" alt="" />
-      </div>
-    )
-  }
+  return (    
+    <div className="alterator" onClick={agregarAlt}> 
+      {
+        (tipo === alterator.trap) && <img src={"../public/trap.png"} className="img_trap" alt="" />
+      }
+      {
+        (tipo === alterator.directioner) &&  <img src={"../public/directioner.png"} className="img_directiorer" alt="" />
+      }
+      {
+        (tipo === alterator.teleport) && <img src={"../public/teleporter.gif"} className="img_teleporter" alt="" />
+      }
+    </div>
+  )
 }
 
+//Paneles de alteradores
 const Panel = ({setAlter}) => {
-  
+
   const handleAlterator = (id) => {
     setAlter(id)
   }
-  return (
-  <section className="modifiers">
-    <h1>Alteradores</h1> 
-    <button onClick={()=>handleAlterator("trap")} value={alterator.trap}>Trap</button>
-    <button onClick={()=>handleAlterator("teleport")}  value={alterator.teleport}>Teleport</button>
-    <button onClick={()=>handleAlterator("directioner")} value={alterator.directioner}>Directioner</button>
-    
-  </section>
 
+  return (
+    <section className="modifiers">
+      <h1>Alteradores</h1> 
+      <button onClick={()=>handleAlterator("trap")} value={alterator.trap}>Trap</button>
+      <button onClick={()=>handleAlterator("teleport")}  value={alterator.teleport}>Teleport</button>
+      <button onClick={()=>handleAlterator("directioner")} value={alterator.directioner}>Directioner</button>
+    </section>
   )
 }
 
@@ -192,7 +183,6 @@ function App() {
       
       <Panel setAlter={setAlter}/>
     </>
-    //TODO: como seleccionar diferentes alteradores y pasarlo como parametros
   )
 }
 
