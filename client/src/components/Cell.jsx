a15c660fe32e0dc9e23cbc005eaecd455c784a87
Ivan Peruchin <ivanperuchin12@gmail.com>
2023-10-11 18:18:45 -0300

    add base and ovni Ref. #60

--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -8,8 +8,18 @@ export const Cell = ({ updateBoard, row, col, children, blue_base, green_base })
     updateBoard(row, col);
   }
 
+  let className = "cell "
+
+  if(row <= green_base.x && col <= green_base.y ){
+    className += "green"
+  }
+
+  if (row >= blue_base.x && col >= blue_base.y) {
+    className += "blue"
+  }
+
   return(
-   <div onClick={handleClick} className="cell" row={row} col={col}>
+   <div onClick={handleClick} className={className} row={row} col={col}>
       {
         <>
           {
@@ -27,10 +37,14 @@ export const Cell = ({ updateBoard, row, col, children, blue_base, green_base })
           { children.alterator && <Alterator tipo={children.alterator}/> }
 
           {
-            (row <= blue_base.x && col <= blue_base.y ) && <div className="base"> Base</div>   
+            <div />  
+          }
+
+          {
+            ((row === 0 && col === 0) && <img src={"../public/green_ovni.png"} className="green_nave" />)
           }
           {
-            (row >= green_base.x && col >= green_base.y) && <div className="base"> Base</div>  
+            ((row === 9 && col === 14) && <img src={"../public/blue_ovni.png"} className="blue_nave" />)
           }
         </>
       }
