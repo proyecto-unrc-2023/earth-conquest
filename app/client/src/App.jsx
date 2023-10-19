ef24410a1f431ac924dbfe41ee367f01ce1a2703
Agustin Lopez <agustinlopezprieto@gmail.com>
2023-10-19 20:46:39 -0300

    #96 Add menu component

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,8 +1,38 @@
+import { useState } from 'react'
 import { Game } from './components/Game/Game'
+import { Menu } from './components/Menu/Menu'
+import { gameStatus } from './constants'
 
 function App () {
+  const [statusGame, setStatusGame] = useState(gameStatus.notStarted)
+  const [gameId, setGameId] = useState(null)
+
+  const createGame = async () => {
+    try {
+      console.log('hice click')
+      const response = await fetch('')
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      setGameId(data.gameId)
+      setStatusGame(gameStatus.started)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
   return (
-    <Game />
+    <>
+      {
+        statusGame === gameStatus.notStarted &&
+          <Menu createGame={createGame} setStatusGame={setStatusGame} />
+      }
+      {
+        statusGame === gameStatus.started &&
+          <Game gameId={gameId} />
+      }
+
+    </>
   )
 }
 
