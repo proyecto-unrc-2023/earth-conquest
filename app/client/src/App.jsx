4e8c8a6c8b151bec75851f3ff72012a0584cd6c2
Agustin Lopez <agustinlopezprieto@gmail.com>
2023-10-21 16:10:41 -0300

    #102 Add connection methods to API

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -6,23 +6,62 @@ import { gameStatus } from './constants'
 function App () {
   const [statusGame, setStatusGame] = useState(gameStatus.notStarted)
   const [gameId, setGameId] = useState(null)
+  const CREATE_GAME = 'http://127.0.0.1:5000/games/create_game'
+  const GET_ALL_GAMES = 'http://127.0.0.1:5000/games/get_all_games'
+  const START_GAME = 'http://127.0.0.1:5000/games/start_game'
 
   const createGame = async () => {
     try {
-      console.log('hice click')
-      const response = await fetch('')
+      const response = await fetch(CREATE_GAME, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        // cache: no-cache, *default, reload, force-cache, only-if-cached
+        // credentials: omit, *same-origin, include
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
       const data = await response.json()
+      console.log(data)
       setGameId(data.gameId)
-      setStatusGame(gameStatus.started)
     } catch (error) {
       console.error('Error fetching data:', error)
     }
   }
+
+  const getAllGames = async () => {
+    try {
+      const response = await fetch(GET_ALL_GAMES)
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.error('Error fetching data: ', error)
+    }
+  }
+
+  const startGame = async () => {
+    try {
+      const response = await fetch(`${START_GAME}/1`, {
+        method: 'PUT'
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.error('Error fetching data: ', error)
+    }
+  }
+
   return (
-    <>
+    <main>
       {
         statusGame === gameStatus.notStarted &&
           <Menu createGame={createGame} setStatusGame={setStatusGame} />
@@ -31,8 +70,9 @@ function App () {
         statusGame === gameStatus.started &&
           <Game gameId={gameId} />
       }
-
-    </>
+      <button onClick={getAllGames}>Get all games</button>
+      <button onClick={startGame}>Start Game</button>
+    </main>
   )
 }
 
