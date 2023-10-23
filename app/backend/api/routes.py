f73caf44f75e3b37370d869c2efdc9282ca92e22
matybq <matybq20@gmail.com>
2023-10-23 17:17:08 -0300

    api changes

--- app/backend/api/routes.py
+++ app/backend/api/routes.py
@@ -8,21 +8,20 @@ class GamesResource(Resource):
     def get(self):
         return GameController.get_all_games()
 
-
-class GameResource(Resource):
-
-    def get(self, game_id):
-        return GameController.get_game_by_id(game_id)
-
     def post(self):
         return GameController.create_new_game()
 
+
+class GameDetails(Resource):
+    def get(self, game_id):
+        return GameController.get_game_by_id(game_id)
+
+
+class StartGame(Resource):
     def put(self, game_id):
         return GameController.start_game(game_id)
 
 
-api.add_resource(GamesResource, '/get_all_games')
-api.add_resource(GameResource,
-                 '/get_game/<int:game_id>',
-                 '/create_game',
-                 '/start_game/<int:game_id>')
+api.add_resource(GamesResource, '/')
+api.add_resource(GameDetails, '/<int:game_id>')     # get details of a game by its id
+api.add_resource(StartGame, '/start_game/<int:game_id>')
