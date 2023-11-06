459a87523c77171e9af74a50d15fe6e84e84b3b3
Delfina Buil <delfinabuil2@gmail.com>
2023-11-06 19:27:50 -0300

    Fixing bug in directioner special case and more testing

--- app/backend/models/board.py
+++ app/backend/models/board.py
@@ -326,20 +326,27 @@ class Board:
                 return directioner.last_pos
             else:
                 if directioner.direction == Direction.LEFT or directioner.direction == Direction.RIGHT:
-                    if self.can_alien_move_to_pos(x - 1, y) or self.can_alien_move_to_pos(x + 1, y):
+                    valid_positions = []
+                    if self.can_alien_move_to_pos(x - 1, y):
+                        valid_positions.append((x-1,y))
+                    if self.can_alien_move_to_pos(x + 1, y):
+                        valid_positions.append((x+1,y))
+                    if valid_positions:
                         # the alien will move to a random adjacent position that's not equal to the snd_pos
-                        new_x, new_y = self.get_adjoining_valid_pos(x, y)
-                        while new_x == directioner.snd_pos[0] and new_y == directioner.snd_pos[1]:
-                            new_x, new_y = self.get_adjoining_valid_pos(x, y)
+                        new_x, new_y = random.choice(valid_positions)
                         return new_x, new_y
                     else:
+                        # the alien has nowhere to move, it'll stay on its position
                         return x, y
                 if directioner.direction == Direction.DOWNWARDS or directioner.direction == Direction.UPWARDS:
-                    if self.can_alien_move_to_pos(x, y - 1) or self.can_alien_move_to_pos(x, y + 1):
+                    valid_positions = []
+                    if self.can_alien_move_to_pos(x, y-1):
+                        valid_positions.append((x,y-1))
+                    if self.can_alien_move_to_pos(x, y+1):
+                        valid_positions.append((x,y+1))
+                    if valid_positions:
                         # the alien will move to a random adjacent position that's not equal to the snd_pos
-                        new_x, new_y = self.get_adjoining_valid_pos(x, y)
-                        while new_x == directioner.snd_pos[0] and new_y == directioner.snd_pos[1]:
-                            new_x, new_y = self.get_adjoining_valid_pos(x, y)
+                        new_x, new_y = random.choice(valid_positions)
                         return new_x, new_y
                     else:
                         return x, y
--- features/steps/directioner_special_case.py
+++ features/steps/directioner_special_case.py
@@ -1,47 +1,33 @@
 from behave import *
 from app.backend.models.alien import Alien
-from app.backend.models.alterator import Alterator
-
-from app.backend.models.board import Board
-from app.backend.models.game import Game
 from app.backend.models.directioner import Directioner
 from app.backend.models.direction import Direction
 from app.backend.models.team import Team
-from app.backend.models.teleporter import Teleporter
-from features.environment import table_to_string
 
 
-
-@given(u'the Directioner is positioned horizontally on the cells (3,12), (3,13) and (3,14)')
-def step_impl(context):
-    context.directioner = Directioner((3,12), Direction.RIGHT)
+@given(u'the Directioner is positioned horizontally on the cells ({x1:d},{y1:d}), ({x2:d},{y2:d}) and ({x3:d},{y3:d})')
+def step_impl(context, x1, y1, x2, y2, x3, y3):
+    context.directioner = Directioner((x1, y1), Direction.RIGHT)
     context.game.board.set_directioner(context.directioner)
 
 
-@given(u'a green alien is positioned on the cell (3,11)')
-def step_impl(context):
+@given(u'a green alien is positioned on the cell ({row:d},{col:d})')
+def step_impl(context, row, col):
     board = context.game.board
     context.alien = Alien(Team.GREEN)
-    board.set_alien(3, 11, context.alien)
+    board.set_alien(row, col, context.alien)
 
 
-@given(u'the alien moves to an adjacent cell, this one being cell (3,12)')
-def step_impl(context):
-    context.game.board.remove_alien_from_board(3, 11, context.alien)
-    context.game.board.set_alien(3, 12, context.alien)
+@given(u'the alien moves to an adjacent cell, this one being cell ({row:d},{col:d})')
+def step_impl(context, row, col):
+    context.game.board.remove_alien_from_board(row, col-1, context.alien)
+    context.game.board.set_alien(row, col, context.alien)
 
 
-@then(u'the alien moves to the cell (3,13)')
-def step_impl(context):
+@then(u'the alien moves to the cell ({row:d},{col:d})')
+def step_impl(context, row, col):
     new_pos = context.game.board.get_alien_position(context.alien)
-    assert(new_pos == (3, 13))
-
-
-@then(u'the alien moves to the cell (3,14)')
-def step_impl(context):
-    new_pos = context.game.board.get_alien_position(context.alien)
-    assert(new_pos == (3, 14))
-
+    assert(new_pos == (row, col))
 
 @then(u'the alien stays on it\'s cell (3,14)')
 def step_impl(context):
