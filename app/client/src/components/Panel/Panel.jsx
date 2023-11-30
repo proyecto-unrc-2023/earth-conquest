a02f4bb73aacd6340be4303932beecc767bd531b
Ivan Peruchin <ivanperuchin12@gmail.com>
2023-11-29 23:28:44 -0300

    Add value of alterators Ref. #183

--- app/client/src/components/Panel/Panel.jsx
+++ app/client/src/components/Panel/Panel.jsx
@@ -2,10 +2,10 @@ import { alterator } from '../../constants.js'
 import { useState } from 'react'
 import './Panel.css'
 
-export const Panel = ({ setAlter, teleporterEnabled }) => {
+export const Panel = ({ setAlter, teleporterEnabled, team }) => {
   const [showDirections, setShowDirections] = useState(false)
-
   const [showAlterators, setshowAlterators] = useState(true)
+  const teamLower = team.toLowerCase()
 
   const handleAlterator = (newAlterator) => {
     if (!teleporterEnabled) return
@@ -31,16 +31,32 @@ export const Panel = ({ setAlter, teleporterEnabled }) => {
           <>
             <button onClick={() => handleAlterator(alterator.TRAP)}>
               <img className='button_img_trap' src='/../trap.png' alt='Trap' />
+              <section className='box-value'>
+                <img src={`../${teamLower}_alien.png`} className='alien' />
+                <p className='value'>4</p>
+              </section>
             </button>
             <button onClick={() => handleAlterator(alterator.TELEPORTER_IN)}>
-              <img className='button_img_teleport1' src='/../teleport_in.gif' alt='Teleporter' />
-              <img className='button_img_teleport1' src='/../teleport_out.gif' alt='Teleporter' />
+              <section>
+                <img className='button_img_teleport1' src='/../teleport_in.gif' alt='Teleporter' />
+                <img className='button_img_teleport1' src='/../teleport_out.gif' alt='Teleporter' />
+              </section>
+              <section className='box-value'>
+                <img src={`../${teamLower}_alien.png`} className='alien' />
+                <p className='value'>6</p>
+              </section>
             </button>
             <button onClick={handleShowDirections}>
-              <img className='button_img_dir1' src='/../directioner_up.png' alt='Directioner' />
-              <img className='button_img_dir1' src='/../directioner_right.png' alt='Directioner' />
-              <img className='button_img_dir1' src='/../directioner_down.png' alt='Directioner' />
-              <img className='button_img_dir1' src='/../directioner_left.png' alt='Directioner' />
+              <section>
+                <img className='button_img_dir1' src='/../directioner_up.png' alt='Directioner' />
+                <img className='button_img_dir1' src='/../directioner_right.png' alt='Directioner' />
+                <img className='button_img_dir1' src='/../directioner_down.png' alt='Directioner' />
+                <img className='button_img_dir1' src='/../directioner_left.png' alt='Directioner' />
+              </section>
+              <section className='box-value'>
+                <img src={`../${teamLower}_alien.png`} className='alien' />
+                <p className='value'>4</p>
+              </section>
             </button>
           </>
         )}
--- app/client/src/components/StatGame/StatsGame.jsx
+++ app/client/src/components/StatGame/StatsGame.jsx
@@ -11,7 +11,7 @@ export const StatsGame = ({ team, lifeOvni, liveAliens, playerName }) => {
       <section className={`${team}_user_info`}>
 
         <section className={`live_alien_${team}`}>
-          <img src={`../${team}_alien.png`} className={`${team}_alien`} alt='' />
+          <img src={`../${team}_alien.png`} className={`${team}_alien`} alt='alien' />
           <p> {liveAliens} </p>
         </section>
 
